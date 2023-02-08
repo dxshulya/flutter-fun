0f2bf4c66dc3c3ee0eac759fae8c1d121c69c495
dxshulya <daraonelove@yandex.ru>
2023-02-08 21:46:25 +0300

    change bottom app bar

--- lib/app/presentation/features/main/pages/main_page.dart
+++ lib/app/presentation/features/main/pages/main_page.dart
@@ -6,9 +6,9 @@ class MainPage extends StatefulWidget {
   static const String navigationPath = '/';
 
   const MainPage(
-    this._tabsSource, {
-    Key? key,
-  }) : super(key: key);
+      this._tabsSource, {
+        Key? key,
+      }) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -34,12 +34,45 @@ class _MainPageState extends State<MainPage> {
       bottomNavigationBar: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          BottomNavigationBar(
-            items: widget._tabsSource.tabs
-                .map((t) => t.toBottomNavigationBarItem())
-                .toList(),
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SafeArea(
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                              const BorderRadius.all(Radius.circular(16)),
+                              border: Border.all(
+                                width: 1.0,
+                                color: Theme.of(context)
+                                    .primaryColor
+                                    .withOpacity(0.1),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  BottomNavigationBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    items: widget._tabsSource.tabs
+                        .map((t) => t.toBottomNavigationBarItem())
+                        .toList(),
+                    currentIndex: _selectedIndex,
+                    onTap: _onItemTapped,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
